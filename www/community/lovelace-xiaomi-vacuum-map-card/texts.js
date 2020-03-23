fc4add188629efd0c9d77e25626532aed92a9897
mefisto22 <torokbalazs9522@gmail.com>
2020-03-23 20:24:41 +0100

    first commit

---
+++ www/community/lovelace-xiaomi-vacuum-map-card/texts.js
@@ -0,0 +1,48 @@
+const mode = "mode";
+const goToTarget = "goToTarget";
+const zonedCleanup = "zonedCleanup";
+const zones = "zones";
+const run = "run";
+const repeats = "repeats";
+const confirmation = "confirmation";
+
+const texts = {
+    "en": {
+        mode: "Mode",
+        goToTarget: "Go to target",
+        zonedCleanup: "Zoned cleanup",
+        zones: "Zones",
+        run: "Start",
+        repeats: "Times:",
+        confirmation: "Command sent!"
+    },
+    "pl": {
+        mode: "Tryb",
+        goToTarget: "Idź do punktu",
+        zonedCleanup: "Czyszczenie strefowe",
+        zones: "Strefy",
+        run: "Uruchom",
+        repeats: "Razy:",
+        confirmation: "Komenda wysłana!"
+    },
+    "es": {
+        mode: "Modos",
+        goToTarget: "Ir al objetivo",
+        zonedCleanup: "Zona de limpieza",
+        zones: "Zonas",
+        run: "Empezar",
+        repeats: "Veces:",
+        confirmation: "Comando enviado!"
+    }
+};
+
+export {
+    mode,
+    goToTarget,
+    zonedCleanup,
+    zones,
+    run,
+    repeats,
+    confirmation,
+    texts
+};
